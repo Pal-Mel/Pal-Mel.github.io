ffd4eec08bb61b946bb1e0d1b2187d1e7de9a761
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2021-10-04 23:21:06 +0300

    main

---
+++ python/lists.py
@@ -0,0 +1,26 @@
+#Коментар для файлу Списків
+# names=["Harry", "Ron", "Hermiona"]
+
+# print(names)
+
+# names.append("Draco")
+
+# print(names)
+
+# names.sort()
+
+# print(names)
+
+# set - немовторні значення
+# s = set()
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.remove(2)
+# print(s)
+
+# len = len(s)
+# print(len)
+
+house ={"Harry":"111", "Draco":"2222"}
+print(house["Harry"])
---
+++ python/square.py
@@ -0,0 +1,7 @@
+from functions import  square
+
+# from functions  импорт всьго модуля. Доступ к функциям через точку
+
+
+for i in range(10) :
+    print(f"x = {i}, square = {square(i)}" )
